a8e0471a107e93068ab6e9addc594b9f6c34478d
Dario Cimafonte <dcimafonte@gmail.com>
2022-04-18 10:48:42 +0100

    BOGOF single-item basket

--- src/test/java/kata/supermarket/offer/BuyOneGetOneFreeOfferTest.java
+++ src/test/java/kata/supermarket/offer/BuyOneGetOneFreeOfferTest.java
@@ -1,6 +1,7 @@
 package kata.supermarket.offer;
 
 import kata.supermarket.Basket;
+import kata.supermarket.Product;
 import org.junit.jupiter.api.Test;
 
 import java.math.BigDecimal;
@@ -9,13 +10,26 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class BuyOneGetOneFreeOfferTest {
 
+    private static final BigDecimal NO_DISCOUNT = new BigDecimal("0.00");
+
     @Test
     void buysOneGetsOneFreeDoesNotApplyOnEmptyBasket() {
         Basket basket = new Basket();
 
         BuyOneGetOneFreeOffer offer = new BuyOneGetOneFreeOffer();
         BigDecimal discount = offer.discountFor(basket);
-        assertEquals(new BigDecimal("0.00"), discount);
+        assertEquals(NO_DISCOUNT, discount);
     }
 
+    @Test
+    void buysOneGetsOneFreeOnSingleItem() {
+        Basket basket = new Basket();
+        BigDecimal pricePerUnit = new BigDecimal("2.49");
+        Product product = new Product(pricePerUnit);
+        basket.add(product.oneOf());
+
+        BuyOneGetOneFreeOffer offer = new BuyOneGetOneFreeOffer();
+        BigDecimal discount = offer.discountFor(basket);
+        assertEquals(NO_DISCOUNT, discount);
+    }
 }
